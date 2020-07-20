3c4155ff59e851fd10a3a2af78ea5b4bebb697fb
Wolfgang Ladermann <extern.ladermann_wolfgang@allianz.de>
2020-07-20 18:08:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # AKS-Selector
+
 This project helps to quickly select from multiple subscriptions your cluster and get the login credentials for AKS
+
+## Requirements
+
+The first thing you need, is `dialog`
+
+I assume that you already have azure-cli installed.
+
+### MacOS
+
+```shell
+brew install dialog
+```
+
+### Linux (Ubuntu)
+
+```shell
+sudo apt install dialog
+```
+
+## How to run
+
+log in via azure cli
+
+```shell
+az login
+```
+
+then run ./aks-select.sh
---
+++ aks-select.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+export SELECTED_SUBSCRIPTION=""
+export SELECTED_AKSCLUSTER=""
+
+export AVAILABLE_AZURE_SUBSCRIPTONS=$(az account list -o tsv | awk '{ printf ("%s %s %s ",$6,$3,$3) }')
+SUBSCRIPTION_COUNT=$("$AVAILABLE_AZURE_SUBSCRIPTONS" | wc -w)
+
+SELECTED_SUBSCRIPTION=$(dialog --radiolist "Select Azure Subscription" 0 0 $SUBSCRIPTION_COUNT $AVAILABLE_AZURE_SUBSCRIPTONS --stdout)
+[ -z $SELECTED_SUBSCRIPTION ] && exit 1
+
+az account set -s $SELECTED_SUBSCRIPTION
+echo "Selected subscription: " $SELECTED_SUBSCRIPTION
+
+export AVAILABLE_AKS_CLUSTER=$(az aks list -o tsv | awk '{ printf ("%s %s %s ",$13,"cluster",$13) }')
+AKS_CLUSTER_COUNT=$("$AVAILABLE_AKS_CLUSTER" | wc -w)
+
+echo "setting dialog"
+export SELECTED_AKS_CLUSTER_NAME=$(dialog --radiolist "Select AKS Cluster" 0 0 $AKS_CLUSTER_COUNT $AVAILABLE_AKS_CLUSTER --stdout)
+[ -z $SELECTED_AKS_CLUSTER_NAME ] && exit 1
+
+echo $SELECTED_AKS_CLUSTER_NAME
+#az aks list --query "[?name=='$SELECTED_AKS_CLUSTER_NAME'].{ResourceGroup:resourceGroup}" -o tsv
+SELECTED_AKS_CLUSTER_RESOURCE_GROUP=$(az aks list --query "[?name=='$SELECTED_AKS_CLUSTER_NAME'].{ResourceGroup:resourceGroup}" -o tsv)
+echo $SELECTED_AKS_CLUSTER_RESOURCE_GROUP
+
+ADMIN=$(dialog --yesno "Get Admin Credentials?" 0 0 --stdout)
+[ -z $ADMIN ] && exit 1
+
+$ADMIN && az aks get-credentials --admin -n $SELECTED_AKS_CLUSTER_NAME -g $SELECTED_AKS_CLUSTER_RESOURCE_GROUP || az aks get-credentials -n $SELECTED_AKS_CLUSTER_NAME -g SELECTED_AKS_CLUSTER_RESOURCE_GROUP
+
